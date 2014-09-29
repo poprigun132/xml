51ee65879d8259cefbfeef46f3badcf94c862348
Sergey Nechiporenko <unreacheble777@gmail.com>
2014-09-29 18:06:53 +0300

    tmp

--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,56 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'Кабинет настройки XML шаблонов';
 ?>
 <div class="site-index">
+	<div class="left-block"><?
+		echo \yii\bootstrap\Nav::widget([
+				'options' => ['class' => 'nav nav-pills'],
+				'items' => [
+					[
+						'label' => '< url >',
+						'url' => ['site/index'],
+						'class'=>['active']
+					],
+					[
+						'label' => '< url >',
+						'url' => ['#'],
+						'class'=>['active']
+					],
+					[
+						'label' => '< url >',
+						'url' => ['#'],
+						'class'=>['active']
+					],
+					[
+						'label' => '< url >',
+						'url' => ['#'],
+						'class'=>['active']
+					],
+					[
+						'label' => '< url >',
+						'url' => ['#'],
+						'class'=>['active']
+					],
+				],
+			]);
+		?>
 
+	</div>
+	<div class="dropdown">
+		<?= \yii\bootstrap\ButtonDropdown::widget([
+				'label' => 'Выбор магазина',
+				'dropdown' => [
+					'options' => [
+						'class'=>'btn-group'
+					],
+					'items' => [
+						['label' => 'DropdownA', 'url' => '/'],
+						['label' => 'DropdownB', 'url' => '#'],
+					],
+				],
+			]);
+		?>
+	</div>
+	<div class="center-block">@@@@@@</div>
 </div>
